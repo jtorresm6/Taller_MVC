937771e98a73e73feee27c3bf1ab255f8f4ccf8c
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-09-02 20:40:25 -0600

    Agrega capa controlador

---
+++ CapaModelo_MVC/Controlador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo_MVC1;
+
+namespace CapaModelo_MVC
+{
+    public class Controlador
+    {
+        Sentencias sentencias = new Sentencias();
+
+        public DataTable llenarDgv(string nombreTabla)
+        {
+            OdbcDataAdapter daControlador = sentencias.llenarTbl(nombreTabla);
+            DataTable dtControlador = new DataTable();
+            daControlador.Fill(dtControlador);
+            return dtControlador;
+        }
+    }
+}
--- CapaModelo_MVC1/Conexion.cs
+++ CapaModelo_MVC1/Conexion.cs
@@ -35,4 +35,4 @@ namespace CapaModelo_MVC1
         }
     }
 }
-}
+
