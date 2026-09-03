b7193aa87b37ffd8b816792b195984a06231c2cb
231091 - JOSE JAVIER TORRES MARTINEZ <jtorresm6@miumg.edu.gt>
2026-09-02 20:35:08 -0600

    Agrega capa modelo (Conexion y Sentencias)

---
+++ CapaModelo_MVC1/Conexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo_MVC1
+{
+    class Conexion
+    {
+        public OdbcConnection conexion()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=BD_ProyectoNominas");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("Error al conectar a la base de datos");
+            }
+            return conn;
+        }
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("Error al desconectar de la base de datos");
+            }
+        }
+    }
+}
+}
